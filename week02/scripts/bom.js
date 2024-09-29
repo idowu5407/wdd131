229f03c463c80be2a961c8f1aab2b15b14e0ad25
idowu5407 <juniormicheal25@gmail.com>
2024-09-30 00:22:34 +0100

    edit

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -11,12 +11,17 @@ button.addEventListener('click', function() {
     //create delete button
   const deleteBtn = document.createElement('button');
   deleteBtn.textContent = '❌';
+
+  const editBtn = document.createElement('button');
+  editBtn.textContent = '🖊';
+
   
   // add input value to li element
   listItem.textContent = input.value;
   
   // append delete button to li element
   listItem.appendChild(deleteBtn);
+  listItem.appendChild(editBtn);
   
   // append li element to unordered list
   list.appendChild(listItem);
@@ -28,4 +33,18 @@ button.addEventListener('click', function() {
   deleteBtn.addEventListener('click', function() {
     list.removeChild(listItem);
   });
+
+  // edit list items
+  editBtn.addEventListener('click', function() {
+    const newValue = prompt('Edit your item:', listItem.firstChild.textContent);
+    if (newValue) {
+      listItem.firstChild.textContent = newValue;
+      
+    }
+    else {
+      alert('Please enter a value!');
+    }
+  });
+     
+  
 });
